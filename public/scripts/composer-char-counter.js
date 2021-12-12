792bbbf28c493617058a6a416f12b1fe5054a302
Shaun Robinson <connecshaun@gmail.com>
2021-12-12 09:52:26 +0000

    Start working on callback whenKeysPressed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,10 +7,13 @@ $(document).ready(function () {
 
   const $typeAreaforTweet = $("#tweet-text")
 
+  $typeAreaforTweet.on("keyup", whenKeysPressed);
 
-  $typeAreaforTweet.on("keyup", function () {
-    console.log($(this).val())
-    console.log($(this).parent().find('.counter'))
-  })
+})
 
-});
+
+const whenKeysPressed = function () {
+  console.log($(this).val())
+  console.log($(this).parent().find('.counter'))
+
+}
