6d2a621250fc7491800671b905935c40e6615410
Shaun Robinson <connecshaun@gmail.com>
2021-12-12 09:35:29 +0000

    Composer-char-counter.js linked to index.html and begin implementing of keyup event

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+//target the <textarea> element
+
+//register event to detect correct length of the input and update the counter
+
+
+$(document).ready(function () {
+
+  const $typeAreaforTweet = $("#tweet-text")
+
+
+  $typeAreaforTweet.on("keyup", function () {
+    console.log($(this).val())
+  })
+
+  // .parent().children()
+
+  // console.log($typeAreaforTweet.parent().children("counter"))
+
+});
