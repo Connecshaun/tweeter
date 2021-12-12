5f2892fbe068247288b80bc64389ea5e68367526
Shaun Robinson <connecshaun@gmail.com>
2021-12-12 11:03:17 +0000

    Finish callback function with character count running and turning red once count drop below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -14,6 +14,8 @@ $(document).ready(function () {
 //callback when event "keyup" takes place
 const whenKeysPressed = function () {
 
+  const $typeAreaforTweet = $("#tweet-text")
+
   const keyed = $(this).val();
 
   //number of characters typed in <textarea>
@@ -22,6 +24,13 @@ const whenKeysPressed = function () {
   //to target the counter 
   const theCounter = ($(this).parent().find('.counter'))
 
-  const remainingChars = 140 - numberOfChars
+  //subtract num of characters typed(keyed.length) from the total permitted(140)
+  const remainingChars = 140 - numberOfChars;
 
-}
+  //if number of characters goes below 0, red class is activated and counter number turns the color of red
+  if (remainingChars < 0) {
+    theCounter.html(remainingChars).addClass("red");
+
+  } else {
+    theCounter.html(remainingChars).removeClass("red");
+  };
