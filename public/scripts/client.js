067e45be2bf86f37f0fb7027d60cd252cb9a9b33
Shaun Robinson <connecshaun@gmail.com>
2021-12-14 06:36:05 +0000

    Create a function to take in a tweet object and append the tweet content to the html doc

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,29 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+//Eventually we'll capture this from the server
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
 
+
+//define a function createTweetElement that takes in a tweet object 
+//responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
+const createTweetElement = function (tweetObject) {
+  const tweet = tweetObject["content"]["text"]
+  return tweet;
+}
+
+$(document).ready(function () {
+  const $tweet = createTweetElement(tweetData)
+
+  $('.textarea-article').append($tweet)
+});
