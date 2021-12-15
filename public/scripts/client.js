449bd380d6deb0e37628b139f001d863fcd079cf
Shaun Robinson <connecshaun@gmail.com>
2021-12-15 23:46:01 +0000

    Shorten code for POST /tweets and loadTweets() and include .then method so that page loads most recent tweet without being refreshed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,76 +5,40 @@
  */
 //Eventually we'll capture this from the server
 
-
 $(document).ready(function () {
 
-  //listen for submit event and prevent default behaviour(page refresh) & create AJAX post request that sends form data to the server
+  //listen for submit event and prevent default behaviour(page refresh)
   $('.target-form').submit(function (event) {
     event.preventDefault();
-    //validate / disallow form submission in the event that the tweet area is empty, or exceeds the 140 character limit.
+
+    //validate tweet
     const typedInTweet = $('#tweet-text').val()
     if (typedInTweet.length > 140) {
       return alert("Tweet is too long. Please shorten.");
     } else if (typedInTweet.length === 0 || typedInTweet === null) {
-      return alert("You did not type your tweet")
+      return alert("You did not type your tweet.")
     }
 
-    $.ajax({
-      type: "POST",
-      url: "/tweets",
-      data: $(this).serialize(),
-      success: () => {
-        console.log("the data has been posted and here is the post", $(this).serialize())
-      },
-      error: (error) => {
-        console.log("the post has failed and this is the error", error)
-      },
-    });
+    //After posting tweet, tweet is loaded via loadTweets()
+    const data = $(this).serialize();
+    $.post("/tweets", data)
+      .then(() => {
+        loadTweets()
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
   });
 
-  // const tweetData = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd"
-  //     },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
 
   // fetch tweets from http://localhost:8080/tweets
   const loadTweets = function () {
-    $.ajax({
-      url: "/tweets",
-      method: "GET",
-      dataType: "json",
-      success: (res) => {
-        renderTweets(res);
-      },
-      error: (error) => {
-        console.log("this request failed and this was the error", error);
-      },
-    });
-
+    $.get("/tweets")
+      .then(data => {
+        renderTweets(data)
+      })
   }
   loadTweets()
-
 
 
   // takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet. 
