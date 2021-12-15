cdccff42163b97ae30825f9e6758fb0be9695b74
Shaun Robinson <connecshaun@gmail.com>
2021-12-15 20:27:52 +0000

    Eslint code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,26 +1,20 @@
 //target the <textarea> element
 
 //register event to detect correct length of the input and update the counter
-
-
-$(document).ready(function () {
-
-  const $typeAreaforTweet = $("#tweet-text")
-
+$(document).ready(function() {
+  const $typeAreaforTweet = $("#tweet-text");
   $typeAreaforTweet.on("keyup", whenKeysPressed);
-
-})
+});
 
 //callback when event "keyup" takes place
-const whenKeysPressed = function () {
-
+const whenKeysPressed = function() {
   const keyed = $(this).val();
 
   //number of characters typed in <textarea>
   const numberOfChars = keyed.length;
 
-  //to target the counter 
-  const theCounter = ($(this).parent().find('.counter'))
+  //to target the counter
+  const theCounter = ($(this).parent().find('.counter'));
 
   //subtract num of characters typed(keyed.length) from the total permitted(140)
   const remainingChars = 140 - numberOfChars;
